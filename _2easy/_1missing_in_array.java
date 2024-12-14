3d096a4768d132092c2f727bca7aa923d2ab7559
vicky kumar <vk4664356@gmail.come>
2024-12-14 12:08:22 +0530

    missing element in array

--- _2easy/_1missing_in_array.java
+++ _2easy/_1missing_in_array.java
@@ -1,5 +1,29 @@
 package _2easy;
 
+import java.util.Scanner;
+
 public class _1missing_in_array {
-    
+    public static int missingNUmber(int arr[] , int n){
+
+        int ans = (n * (n + 1)/2);
+        for(int i = 0; i<arr.length; i++){
+            ans -= arr[i];
+        }
+        return ans;
+    }
+       public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of the array : ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("enter the array element : ");
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int missing_number = missingNUmber(arr, n);
+        System.out.println("missing number is = " + missing_number);
+
+       }
+
 }

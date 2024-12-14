6d635f383de6f4d2915799b26723b14124dec4b6
vicky kumar <vk4664356@gmail.come>
2024-12-14 12:19:50 +0530

    indexof the subarray sum

--- _2easy/_1missing_in_array.java
+++ _2easy/_1missing_in_array.java
@@ -6,23 +6,24 @@ public class _1missing_in_array {
     public static int missingNUmber(int arr[] , int n){
 
         int ans = (n * (n + 1)/2);
-        for(int i = 0; i<arr.length; i++){
+        for(int i = 0; i<arr.length - 1; i++){
             ans -= arr[i];
         }
         return ans;
     }
        public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter the size of the array : ");
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        System.out.println("enter the array element : ");
-        for(int i = 0; i<n; i++){
-            arr[i] = sc.nextInt();
-        }
+        Scanner sc = new Scanner(System.in)
+            System.out.println("enter the size of the array : ");
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            System.out.println("enter the array element : ");
+            for(int i = 0; i<n; i++){
+                arr[i] = sc.nextInt();
+            }
 
-        int missing_number = missingNUmber(arr, n);
-        System.out.println("missing number is = " + missing_number);
+            int missing_number = missingNUmber(arr, n);
+            System.out.println("missing number is = " + missing_number);
+        
 
        }
 
--- _3medium/_1indexs_of_subArray_sum.java
+++ _3medium/_1indexs_of_subArray_sum.java
@@ -1,5 +1,44 @@
 package _3medium;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class _1indexs_of_subArray_sum {
+
+    public static ArrayList<Integer> subarraySum(int arr[] , int n , int sum){
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        int j = 0;
+        int current_sum = 0;
+
+        for(int i = 0; i<n; i++){
+            current_sum += arr[i];
+            while(current_sum > sum && j < i){
+
+                sum -= arr[j];
+                j++;
+            }
+            if(current_sum == sum){
+                list.add(j + 1);
+                list.add(i + 1);
+                break;
+            }
+        }
+        if(list.isEmpty()){
+            list.add(-1);
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of the array : ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("enter the array element : ");
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+    }
     
 }
